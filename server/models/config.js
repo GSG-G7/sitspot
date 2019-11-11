c78753c4e77e3c972db5161066507c4dbd2079fd
amoodaa <amoodaa@gmail.com>
2019-11-11 14:40:56 +0200

    wip

--- server/models/config.js
+++ server/models/config.js
@@ -1,9 +1,5 @@
 const Airtable = require('airtable');
 
-if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test')
-  // eslint-disable-next-line global-require, import/no-unresolved
-  require('env2')('.env');
-
 Airtable.configure({
   endpointUrl: 'https://api.airtable.com',
   apiKey: process.env.AIRTABLE_KEY,
